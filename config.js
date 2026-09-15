bbaaa76c77e9153d7b95b5efb9e8e8aaa391d079
Florian Hoss <mail@florianhoss.de>
2026-09-15 10:44:23 +0200

    Use native GitHub App identity, drop ntfy notifications

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 module.exports = {
     platform: 'github',
     token: process.env.RENOVATE_TOKEN,
-    gitAuthor: `${process.env.RENOVATE_AUTHOR} <${process.env.RENOVATE_PRIVATE_EMAIL}>`,
-    username: process.env.RENOVATE_USERNAME,
     repositories: ['flohoss/docker-compose', 'flohoss/stacks'],
     onboarding: false,
     requireConfig: 'optional',
@@ -19,11 +17,4 @@ module.exports = {
             password: process.env.RENOVATE_GHCR_TOKEN,
         }
     ],
-    allowedCommands: ['^curl -s -X POST "' + process.env.RENOVATE_NTFY_URL + '" .*'],
-    postUpgradeTasks: {
-        commands: [
-            `curl -s -X POST "${process.env.RENOVATE_NTFY_URL}" -H "Authorization: Bearer ${process.env.RENOVATE_NTFY_TOKEN}" -H "X-Tags: twisted_rightwards_arrows" -H "X-Title: Renovate - {{{depName}}}" -d "{{{prTitle}}}"`
-        ],
-        executionMode: 'branch'
-    }
 };
